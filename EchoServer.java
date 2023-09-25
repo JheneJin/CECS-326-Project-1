2072e7939b9214836ea090ff7c99b887e4df6519
JheneJin <80924562+JheneJin@users.noreply.github.com>
2023-09-24 19:17:32 -0700

    Update EchoServer.java

--- EchoServer.java
+++ EchoServer.java
@@ -11,33 +11,32 @@ import java.io.*;
 public class  EchoServer{
     //goes to the client side
     public static void main(String[] args) throws IOException {
-	    
+
         try {
         //creates server socket
-	ServerSocket ss = new ServerSocket(6007);
+		ServerSocket ss = new ServerSocket(6007);
         // connects both client and server sockets initialized as s(not to be confused with the other socket s on the client side)
         Socket s = ss.accept();
         //when it connects this message is prompted
         System.out.println("client connected");
-        // send data to the socket's output stream(server), going to the client side
+        // send data from the socket's output stream(server), going to the client side
         PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-        //recieve data to the socket's input stream(client), coming to the server side
+        //send data to the socket's input stream(server), coming from the client side
         BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
         String clientInput; 
         //loops echos between the server and client side until the user enters LEAVE or enters a blank space
-        //gets the client's input
+        //gets the client's input(client output stream) into the server socket input streamm
         while((clientInput = in.readLine()) != "LEAVE"){
             //prints out the clientInput on the server
             System.out.println("On Server :" + clientInput);
-            //echos back to the client
+            //echos back to the client from the server side
             out.println(clientInput);
             }
         } 
-		
+
         //catches socket closed error, and makes the console output more clear
         catch (SocketException e) {
             System.out.println("Client disconnected because they entered the word LEAVE");
         }   
-	    
 	}
 }
